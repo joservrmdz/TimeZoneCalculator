e73e7cb35e53e202f08faaa77985b15ee3f50071
Jose Ramon Rivera <jose.rivera@ocado.com>
2020-03-17 01:42:49 +0100

    Removed dependencies for GroovyMarkupTemplate

--- src/test/groovy/TimeZoneCalculatorSpec.groovy
+++ src/test/groovy/TimeZoneCalculatorSpec.groovy
@@ -1,60 +1,77 @@
 import groovy.json.JsonSlurper
 import org.timezonecalculator.Server
+import org.timezonecalculator.timezonedb.transport.Results
+import ratpack.http.client.ReceivedResponse
 import ratpack.test.MainClassApplicationUnderTest
 import spock.lang.Shared
 import spock.lang.Specification
 
 class TimeZoneCalculatorSpec extends Specification {
 
-    @Shared
     def mainClassApplicationUnderTest = new MainClassApplicationUnderTest(Server.class)
-
-    def "starts the web application"() {
-
-        when:
-        def statusCode = mainClassApplicationUnderTest.httpClient.get().statusCode
-
-        then:
-        statusCode == 200
-
-    }
+    @Shared
+    def json = new JsonSlurper()
 
     def "returns CET timezone for Barcelona, Spain"() {
-        given:
-        def json = new JsonSlurper()
-
         when:
-        def response = mainClassApplicationUnderTest.httpClient.get("time?city=Barcelona&country=ES")
+        def response = getTimeZone "time?city=Barcelona&country=ES"
 
         then:
-        String TZ = json.parseText(response.body.text).getAt("zones").getAt("abbreviation")
-        TZ == "[CET]"
+        def result = json.parseText(response.body.text) as Results
+        result.zones[0].abbreviation == "CET"
     }
 
-    def "returns 'Please type in a city.' when the city parameter is missing."(){
-        given:
-        def json = new JsonSlurper()
-
+    def "returns message: 'Please type in a city.' when the city parameter is missing."() {
         when:
-        def response = mainClassApplicationUnderTest.httpClient.get("time?city=&country=US")
+        def response = getTimeZone "time?city=&country=US"
 
         then:
-        def results = json.parseText(response.body.text)
-        results.errorMessage ==  "Please type in a city."
+        json.parseText(response.body.text).errorMessage == "Please type in a city."
 
     }
 
-    def "returns 'Record not found' when the city doesn't exist"(){
-        given:
-        def json = new JsonSlurper()
-
+    def "returns message: 'Record not found' when the city doesn't exist"() {
         when:
-        def response = mainClassApplicationUnderTest.httpClient.get("time?city=Barcelona&country=HN")
+        def response = getTimeZone("time?city=Barcelona&country=HN")
 
         then:
-        def results = json.parseText(response.body.text)
-        results.errorMessage ==  "Record not found."
+        def errorResponse = json.parseText(response.body.text)
+        errorResponse.errorMessage == "Record not found."
 
+    }
+
+    def "returns 'WET' as Timezone for Las Palmas, Spain"() {
+        when:
+        def response = getTimeZone "time?city=Las+Palmas&country=ES"
+
+        then:
+        def results = json.parseText(response.body.text) as Results
+        results.zones[0].abbreviation == "WET"
+
+    }
+
+    def "returns 'HN' as Timezone for Tegucigalpa, Honduras"() {
+        when:
+        def response = getTimeZone "time?city=Tegucigalpa&country=HN"
+
+        then:
+        def results = json.parseText(response.body.text) as Results
+        results.zones[0].abbreviation == "CST"
+
+    }
+
+    def "returns 10 results for Springfield, US"() {
+        when:
+        def response = getTimeZone "time?city=Springfield&country=US"
+
+        then:
+        def results = json.parseText(response.body.text) as Results
+        results.zones.size() == 10
+
+    }
+
+    private ReceivedResponse getTimeZone(String query) {
+        mainClassApplicationUnderTest.httpClient.get(query)
     }
 
 
